a96ab98639ed9ca5c9804bd88c20afdc5a6c4c8e
Jamshid91 <iymon0217@gmail.com>
2022-08-30 00:33:59 +0500

    added search filter function

--- assets/js/header.js
+++ assets/js/header.js
@@ -5,7 +5,43 @@ const headerBurger = document.querySelector('.header-burger'),
       headerSearch = document.querySelector('.header-search'),
       search = document.querySelector('.search'),
       searchClose = document.querySelector('.search__close'),
-      body = document.querySelector('body');
+      body = document.querySelector('body'),
+      searchInp = document.querySelector('.search__inp'),
+      resultTitle = document.querySelector('.search-result__text'),
+      searchResults = document.querySelectorAll('.search-item__title');
+
+
+
+
+document.querySelector('.search__inp').oninput = function () {
+ let val = this.value.trim();
+    if(val != '') {
+        resultTitle.classList.add('d-none')
+        searchResults.forEach(elem => {
+            if(elem.innerText.search(val) == -1) {
+                elem.innerHTML = elem.innerText; 
+                elem.parentElement.classList.remove('hide')
+            } else {
+                elem.parentElement.classList.add('hide');
+                let str = elem.innerText;
+                elem.innerHTML = insertMark(str, elem.innerText.search(val), val.length)
+            }
+        });
+ 
+    }
+    else {
+        resultTitle.classList.remove('d-none')
+        searchResults.forEach(elem => {
+            elem.parentElement.classList.remove('hide');
+            elem.innerHTML = elem.innerText;
+        });
+    }
+    
+}
+     
+function insertMark(string,pos,len) {
+    return string.slice(0,pos) + "<span class='mark'>"+string.slice(pos,pos+len) +"</span>"+string.slice(pos+len)
+};
 
 
 
@@ -22,18 +58,18 @@ headerLang.addEventListener('click', () => {
 });
 
 headerSearch.addEventListener('click', () => {
-    search.classList.remove('d-none');
+    search.classList.add('showSearch');
     body.classList.add('overflow-hidden');
 });
 
 searchClose.addEventListener('click', () => {
-    search.classList.add('d-none');
+    search.classList.remove('showSearch');
     body.classList.remove('overflow-hidden');
 });
 
 window.onclick = function(e) {
     if(e.target == search) {
-        search.classList.add('d-none');
+        search.classList.remove('showSearch');
         body.classList.remove('overflow-hidden'); 
     }
 }
@@ -43,3 +79,5 @@ $('.menu-title').click(function() {
     $(this).siblings().removeClass('showMenuList');
     $(this).toggleClass('showMenuList');
 });
+
+
